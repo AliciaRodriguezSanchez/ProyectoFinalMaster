47bb86cae4ca8d01426f5b330d726cc7e4f29d90
Dani Taillefer <danieltaillefernieto@gmail.com>
2026-06-10 20:50:41 +0200

    feature: cardcomponents

--- Front/brickswap/src/app/app.ts
+++ Front/brickswap/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardPanelComponent } from './shared/card-panel/card-panel.component';
+import { InfoCardComponent } from './shared/info-card/info-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardPanelComponent, InfoCardComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
