83fd910bfd9e67d5ca2057698715ef48a38bbed4
Dani Taillefer <danieltaillefernieto@gmail.com>
2026-06-11 20:25:59 +0200

    feature: table-component

--- Front/brickswap/src/app/shared/table/table.component.ts
+++ Front/brickswap/src/app/shared/table/table.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { IPendingTable } from '../../interfaces/ipending-table.interface';
+import { IUsersTable } from '../../interfaces/iusers-table.interface';
+
+export type TableItem = IUsersTable | IPendingTable;
 
 @Component({
   selector: 'app-table',
@@ -7,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './table.component.css',
 })
 export class TableComponent {
+  typeTable = input<'pendientes' | 'usuarios'>('pendientes');
+  data = input<TableItem[]>([]); 
+
+  getBorderClass(type: string | undefined, isFirst: boolean): string {
+    if (!isFirst) return 'border-transparent';
+    if (type === 'pendientes') return 'border-warning';
+    if (type === 'usuarios') return 'border-success'; 
+    return 'border-transparent';
+  }
 
 }
