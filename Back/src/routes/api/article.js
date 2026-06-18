0e80c7381b51a064f29fa58a7f51192284ddc967
Alicia <alicia.rodriguez.sanchez.as@gmail.com>
2026-06-18 21:02:15 +0200

    reestructurar routes/api

--- Back/src/routes/api/article.js
+++ Back/src/routes/api/article.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const articleController = require('../controllers/article.controller');
+const articleController = require('../../controllers/article.controller');
 
 
 // GET
