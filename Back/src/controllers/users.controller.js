ff9691c7a150e3875e8d751acb923125625afdf1
Álvaro Moreno <alvaromorenomad@gmail.com>
2026-06-20 22:20:26 +0200

    programada la api de registro

--- Back/src/controllers/users.controller.js
+++ Back/src/controllers/users.controller.js
@@ -1,9 +1,73 @@
+const bcrypt = require('bcryptjs');
+const userModel = require('../models/users.model')
 
-
-const register = (req, res) => {
+const register = async (req, res) => {
+    try {
     
+        const { name, lastname, username, email, password, repetPassword } = req.body;
 
+        if(!name || !lastname || !username || !email || !password || !repetPassword){
+            return res.status(400).json({ message: 'Todos los campos son obligatorios' })
+        }
+
+        if(password !== repetPassword){
+            return res.status(400).json({ message: 'Las contraseñas no coinciden' });
+        }
+
+        const user = await userModel.selectByEmail(email)
+
+        if(user){
+            return res.status(409).json({
+                message: 'Ese email ya está registrado'
+            })
+        }
+
+        const usernameExist = await userModel.selectByUsername(username)
+
+        if(usernameExist){
+            return res.status(409).json({
+                message: 'Ese nombre de usuario ya está registrado'
+            })
+        }
+
+        const hashedPassword = bcrypt.hashSync(password, 8);
+        const formattedName = capitalizar(name);
+        const formattedLastname = capitalizar(lastname);
+
+
+        const newUser = await userModel.insert({
+            name: formattedName,
+            lastname: formattedLastname,
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        const userWithoutPassword = {
+            id: newUser.insertId,
+            name: formattedName,
+            lastname: formattedLastname,
+            username,
+            email,
+        };
+
+        res.status(201).json(userWithoutPassword);
+
+    }catch (error) {
+            res.status(500).json({ message: error.message });
+        }
 } 
+
+
+////////////////funciones auxiliares
+
+const capitalizar = (texto) => {
+    return texto
+        .toLowerCase()
+        .split(' ')
+        .map(palabra => palabra.charAt(0).toUpperCase() + palabra.slice(1))
+        .join(' ');
+}
 
 module.exports = {
     register
--- Back/src/models/users.model.js
+++ Back/src/models/users.model.js
@@ -1,7 +1,34 @@
-const insert = () => {
+const db = require('../config/db');
 
+const insert = async ({ name, username, lastname, email, password}) => {
+    const [result] = await db.query(
+        'INSERT INTO perfiles (nombre, apellidos, nombre_usuario, email, contraseña, rol_id) VALUES (?, ?, ?, ?, ?, ?)',
+        [name, lastname, username, email, password, 1]
+    );
+
+    return result;
+}
+
+const selectByEmail = async(email) =>{
+    const [result] = await db.query(
+        'SELECT * FROM perfiles WHERE email = ?',
+        [email]
+    )
+
+    return result[0] || null;
+}
+
+const selectByUsername = async(username) =>{
+    const [result] = await db.query(
+        'SELECT * FROM perfiles WHERE nombre_usuario = ?',
+        [username]
+    )
+
+    return result[0] || null;
 }
 
 module.exports = {
-    insert
+    insert,
+    selectByEmail,
+    selectByUsername
 }
