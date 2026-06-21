ebd79161b47ad628c586dc47e8cabfe660be80d7
Dani Taillefer <danieltaillefernieto@gmail.com>
2026-06-21 17:40:18 +0200

    Api de panel de moderación creada

--- Back/src/controllers/report.controller.js
+++ Back/src/controllers/report.controller.js
@@ -1,5 +1,96 @@
 const Report = require('../models/report.model');
 
+// GET /api/reports?estado=pendiente G2
+const pendingReports = async (req, res) => {
+    try{
+        const state = req.query.estado;
+        const reports = await Report.getAllPendingReports(state);
+        res.json(reports);
+    }catch (error){
+        console.log("ERROR REAL:", error);
+        res.status(500).json({
+            message: 'Error al cargar los Reportes pendientes'
+        });
+    }
+};
+
+// GET /api/reports?estado=Revisado_Retirado G2
+const retireReports = async (req, res) => {
+    try{
+        const state = req.query.estado;
+        const reports = await Report.getAllRetireReports(state);
+        res.json(reports);
+    }catch (error){
+        console.log("ERROR REAL:", error);
+        res.status(500).json({
+            message: 'Error al cargar los Reportes retirados'
+        });
+    }
+};
+
+// GET /api/reports?estado=Revisado_Mantenido G2
+const matainReports = async (req, res) => {
+    try{
+        const state = req.query.estado;
+        const reports = await Report.getAllMantainReports(state);
+        res.json(reports);
+    }catch (error){
+        console.log("ERROR REAL:", error);
+        res.status(500).json({
+            message: 'Error al cargar los Reportes mantenidos'
+        });
+    }
+};
+
+// GET /api/reports/:id G2
+const reportsById = async (req, res) => {
+    try{
+        const id = req.params.id;
+        const report = await Report.getReportsById(id);
+        if(!report){
+            res.status(404).json({
+                message: 'Reporte no encontrado'
+            })
+        }
+        res.json(report);
+    }catch (error){
+        console.log("ERROR REAL:", error);
+        res.status(500).json({
+            message: 'Error al cargar el reporte'
+        });
+    }
+};
+
+// GET /api/reports/stadistics G2
+const stateStadistics = async (req, res) => {
+    try{
+        const stadistics = await Report.getAllStadicticsState();
+        res.json(stadistics);
+    }catch (error){
+        console.log("ERROR REAL:", error);
+        res.status(500).json({
+            message: 'Error al cargar las estadísticas de estado'
+        });   
+    }
+}
+
+// PUT /api/reports/:id
+const updateReportStatus = async (req, res) => {
+    try{
+        const id = req.params.id;
+        const { estado_reporte, resolucion_comentario} = req.body;
+        const report = await Report.updateReportState(id, estado_reporte, resolucion_comentario);
+        res.status(200).json({
+            message: `Reporte actualizado al estado ${estado_reporte}`
+        })
+    }catch (error){
+        console.log("ERROR REAL:", error);
+        res.status(500).json({
+            message: 'Error al actualizar el estado del reporte'
+        });  
+    }
+};
+
 // POST /api/reports REPORTAR ARTÍCULO
 const createReport = async (req, res) => {
     try {
@@ -41,5 +132,5 @@ const createReport = async (req, res) => {
 };
 
 module.exports = {
-    createReport
+    createReport, pendingReports, reportsById, updateReportStatus, stateStadistics
 };
--- Back/src/models/report.model.js
+++ Back/src/models/report.model.js
@@ -1,5 +1,79 @@
 const db = require('../config/db');
 
+// GET REPORTED ID G2
+//articulo, motivo, reportado por, img articulo.
+const getReportsById = async (id) => {
+    const [result] = await db.query(`
+        SELECT a.titulo, r.motivo, p.nombre, a.foto
+        FROM reportes as r 
+        JOIN articulos as a ON r.articulo_id = a.id
+        JOIN perfiles as p ON r.denunciante_id = p.id
+        WHERE r.id = ?
+        `, [parseInt(id)]);
+        return result[0];
+}
+
+// GET REPORTES PENDIENTES G2
+// NOMBRE DEL ARTICULO (TABLA ARTICULOS), MOTIVO, FECHA REPORTE Y ESTADO DE REPORTE
+
+const getAllPendingReports = async (estado) => {
+    const [result] = await db.query( `
+    SELECT a.titulo, r.motivo, r.fecha_reporte, r.estado_reporte
+    FROM reportes AS r
+    JOIN  articulos AS a ON r.articulo_id =  a.id
+    WHERE r.estado_reporte = ?
+    `, [estado]);
+    return result;
+};
+
+// GET REPORTES Revisado_Retirado G2
+// NOMBRE DEL ARTICULO (TABLA ARTICULOS), MOTIVO, FECHA REPORTE Y ESTADO DE REPORTE
+
+const getAllRetireReports = async (estado) => {
+    const [result] = await db.query( `
+    SELECT a.titulo, r.motivo, r.fecha_reporte, r.estado_reporte
+    FROM reportes AS r
+    JOIN  articulos AS a ON r.articulo_id =  a.id
+    WHERE r.estado_reporte = ?
+    `, [estado]);
+    return result;
+};
+
+// GET REPORTES Revisado_Mantenido G2
+// NOMBRE DEL ARTICULO (TABLA ARTICULOS), MOTIVO, FECHA REPORTE Y ESTADO DE REPORTE
+
+const getAllMantainReports = async (estado) => {
+    const [result] = await db.query( `
+    SELECT a.titulo, r.motivo, r.fecha_reporte, r.estado_reporte
+    FROM reportes AS r
+    JOIN  articulos AS a ON r.articulo_id =  a.id
+    WHERE r.estado_reporte = ?
+    `, [estado]);
+    return result;
+};
+
+// GET ESTADISTICAS POR ESTADO G2
+// ESTADO_REPORTE
+
+const getAllStadicticsState = async () => {
+    const [result] = await db.query(`
+        SELECT estado_reporte AS Estado, COUNT(*) AS total_reportes
+        FROM reportes 
+        GROUP BY estado_reporte
+        `);
+    return result;
+}
+
+// UPDATE Actualizar estado del reporte G2
+const updateReportState = async (id,estado,comentario) => {
+    const [result] = await db.query(`
+        UPDATE reportes
+        SET estado_reporte = ?, resolucion_comentario = ? 
+        WHERE id = ?
+        `, [estado, comentario, parseInt(id)]);
+    return result;
+}
+
 // CREAR REPORTE
 const createReport = async (reportData) => {
 
@@ -29,5 +103,5 @@ const createReport = async (reportData) => {
 };
 
 module.exports = {
-    createReport
+    createReport, getAllPendingReports, getAllRetireReports, getAllMantainReports,getAllStadicticsState, getReportsById, updateReportState
 };
